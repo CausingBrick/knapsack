d2cc46a89a0f88c9ad2aa5f41623c247478b29d8
CausingBrick <1520593807@qq.com>
2021-04-20 11:05:08 +0800

    Optimal the logical

--- knapsack01/backtrack.go
+++ knapsack01/backtrack.go
@@ -7,13 +7,17 @@ func Backtrack(profits []int, weights []int, capacity int) int {
 
 func backtrack(pft, wgt []int, i, cap int) int {
 	value := 0
-	if i == len(pft)-1 {
-		value = 0
-	} else if cap < wgt[i] {
+	switch {
+	case i == len(pft)-1:
+		break
+	case cap < wgt[i]:
 		value = backtrack(pft, wgt, i+1, cap)
-	} else {
-		value = max(backtrack(pft, wgt, i+1, cap),
-			backtrack(pft, wgt, i+1, cap-wgt[i])+pft[i])
+	default:
+		value = max(
+			backtrack(pft, wgt, i+1, cap),
+			backtrack(pft, wgt, i+1, cap-wgt[i])+pft[i],
+		)
+
 	}
 	return value
 }
