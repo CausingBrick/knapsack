48cdaad28c88e60f8dc266c911a9973f88243dd7
CausingBrick <1520593807@qq.com>
2021-04-24 14:01:17 +0800

    Opt the test func

--- twodeminsion/backtrack_test.go
+++ twodeminsion/backtrack_test.go
@@ -4,17 +4,19 @@ import (
 	"testing"
 )
 
-var bkSets = []*dataset{
-	datasetNew("set05", "./testdata/"),
-	datasetNew("set20", "./testdata/"),
-	// datasetNew("set40", "./testdata/"),
-	// datasetNew("set100", "./testdata/"),
+var bkSets = map[string]*dataset{
+	"set05": datasetNew("set05", "./testdata/"),
+	"set20": datasetNew("set20", "./testdata/"),
 }
 
 func TestBacktrack(t *testing.T) {
 	for _, set := range bkSets {
-		if got := Backtrack(set.its, set.kp); got != set.slt {
-			t.Errorf("Want %d, got %d\n", set.slt, got)
-		}
+		set := set //Capturing iterator variables
+		t.Run(set.name, func(t *testing.T) {
+			if got := Backtrack(set.its, set.kp); got != set.slt {
+				t.Parallel()
+				t.Errorf("Set %s Want %d, got %d\n", set.name, set.slt, got)
+			}
+		})
 	}
 }
--- twodeminsion/dynamic_test.go
+++ twodeminsion/dynamic_test.go
@@ -4,33 +4,83 @@ import (
 	"testing"
 )
 
-var dynamicSets = []*dataset{
-	datasetNew("set05", "./testdata/"),
-	datasetNew("set20", "./testdata/"),
-	datasetNew("set40", "./testdata/"),
-	datasetNew("set100", "./testdata/"),
+var dynSets = map[string]*dataset{
+	"set05":  datasetNew("set05", "./testdata/"),
+	"set20":  datasetNew("set20", "./testdata/"),
+	"set40":  datasetNew("set40", "./testdata/"),
+	"set100": datasetNew("set100", "./testdata/"),
+	"set250": datasetNew("set250", "./testdata/"),
+	"set500": datasetNew("set500", "./testdata/"),
 }
 
 func TestDynamicRec(t *testing.T) {
-	for _, set := range dynamicSets {
-		if got := DynamicRec(set.its, set.kp); got != set.slt {
-			t.Errorf("Set %s Want %d, got %d\n", set.name, set.slt, got)
+	t.Parallel()
+	t.Run(dynSets["set05"].name, func(t *testing.T) {
+		if got := DynamicRec(dynSets["set05"].its, dynSets["set05"].kp); got != dynSets["set05"].slt {
+			t.Parallel()
+			t.Errorf("Set %s Want %d, got %d\n", dynSets["set05"].name, dynSets["set05"].slt, got)
 		}
-	}
+	})
+	t.Run(dynSets["set20"].name, func(t *testing.T) {
+		if got := DynamicRec(dynSets["set20"].its, dynSets["set20"].kp); got != dynSets["set20"].slt {
+			t.Parallel()
+			t.Errorf("Set %s Want %d, got %d\n", dynSets["set20"].name, dynSets["set20"].slt, got)
+		}
+	})
+	t.Run(dynSets["set40"].name, func(t *testing.T) {
+		if got := DynamicRec(dynSets["set40"].its, dynSets["set40"].kp); got != dynSets["set40"].slt {
+			t.Parallel()
+			t.Errorf("Set %s Want %d, got %d\n", dynSets["set40"].name, dynSets["set40"].slt, got)
+		}
+	})
 }
 
 func TestDynamic(t *testing.T) {
-	for _, set := range dynamicSets {
-		if got := Dynamic(set.its, set.kp); got != set.slt {
-			t.Errorf("Set %s Want %d, got %d\n", set.name, set.slt, got)
+	t.Parallel()
+	t.Run(dynSets["set05"].name, func(t *testing.T) {
+		if got := Dynamic(dynSets["set05"].its, dynSets["set05"].kp); got != dynSets["set05"].slt {
+			t.Parallel()
+			t.Errorf("Set %s Want %d, got %d\n", dynSets["set05"].name, dynSets["set05"].slt, got)
 		}
-	}
+	})
+	t.Run(dynSets["set20"].name, func(t *testing.T) {
+		if got := Dynamic(dynSets["set20"].its, dynSets["set20"].kp); got != dynSets["set20"].slt {
+			t.Parallel()
+			t.Errorf("Set %s Want %d, got %d\n", dynSets["set20"].name, dynSets["set20"].slt, got)
+		}
+	})
+	t.Run(dynSets["set40"].name, func(t *testing.T) {
+		if got := Dynamic(dynSets["set40"].its, dynSets["set40"].kp); got != dynSets["set40"].slt {
+			t.Parallel()
+			t.Errorf("Set %s Want %d, got %d\n", dynSets["set40"].name, dynSets["set40"].slt, got)
+		}
+	})
 }
 
 func TestDynamicCompress(t *testing.T) {
-	for _, set := range dynamicSets {
-		if got := DynamicCompress(set.its, set.kp); got != set.slt {
-			t.Errorf("Set %s Want %d, got %d\n", set.name, set.slt, got)
+	t.Parallel()
+	t.Run(dynSets["set05"].name, func(t *testing.T) {
+		if got := DynamicCompress(dynSets["set05"].its, dynSets["set05"].kp); got != dynSets["set05"].slt {
+			t.Parallel()
+			t.Errorf("Set %s Want %d, got %d\n", dynSets["set05"].name, dynSets["set05"].slt, got)
 		}
-	}
+	})
+	t.Run(dynSets["set20"].name, func(t *testing.T) {
+		if got := DynamicCompress(dynSets["set20"].its, dynSets["set20"].kp); got != dynSets["set20"].slt {
+			t.Parallel()
+			t.Errorf("Set %s Want %d, got %d\n", dynSets["set20"].name, dynSets["set20"].slt, got)
+		}
+	})
+	t.Run(dynSets["set40"].name, func(t *testing.T) {
+		if got := DynamicCompress(dynSets["set40"].its, dynSets["set40"].kp); got != dynSets["set40"].slt {
+			t.Parallel()
+			t.Errorf("Set %s Want %d, got %d\n", dynSets["set40"].name, dynSets["set40"].slt, got)
+		}
+	})
+	t.Run(dynSets["set100"].name, func(t *testing.T) {
+		if got := DynamicCompress(dynSets["set100"].its, dynSets["set100"].kp); got != dynSets["set100"].slt {
+			t.Parallel()
+			t.Errorf("Set %s Want %d, got %d\n", dynSets["set100"].name, dynSets["set100"].slt, got)
+		}
+	})
 }
